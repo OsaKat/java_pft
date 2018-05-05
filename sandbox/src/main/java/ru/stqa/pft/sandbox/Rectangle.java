0ce2fb56439af10f232917524c3e8c93d453b67b
OsaKat <ekaterinaospennikova@gmail.com>
2018-05-05 23:35:49 +0300

    Классы, методы, функции

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Rectangle.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.sandbox;
+
+/**Created by Kate on 05.05.2018
+ */
+
+public class Rectangle {
+    public double a;
+    public double b;
+
+    public Rectangle(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public double area() {
+        return this.a * this.b;
+    }
+}
