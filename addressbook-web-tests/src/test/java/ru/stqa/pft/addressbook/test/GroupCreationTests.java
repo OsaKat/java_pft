76a236a4e796465e519f5e2949a94cf339579685
OsaKat <ekaterinaospennikova@gmail.com>
2018-05-13 23:35:57 +0300

    Создан общий базовый класс для классов-помощников

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/GroupCreationTests.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.addressbook.test;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupCreationTests  extends TestBase {
+
+    @Test
+    public void testGroupCreation() {
+        app.getNavigationHelper().gotoGroupPage("groups");
+        app.getGroupHelper().initGroupCreation("new");
+        app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+        app.getGroupHelper().submitGroupCreation("submit");
+        app.getGroupHelper().returnToGroupPage("group page");
+    }
+
+}
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/GroupDeletionTests.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.addressbook.test;
+
+import org.testng.annotations.Test;
+
+public class GroupDeletionTests extends TestBase {
+
+
+    @Test
+    public void testGroupDeletion() {
+        app.getNavigationHelper().gotoGroupPage("groups");
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().deleteSelectedGroups();
+        app.getGroupHelper().returnToGroupPage("group page");
+    }
+
+}
