1b0341af6ccc2b3f069a7040a8b48066a7a9e749
OsaKat <ekaterinaospennikova@gmail.com>
2018-05-21 00:30:59 +0300

    Запуск тестов в разных браузерах

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/TestBase.java
@@ -1,14 +1,15 @@
 package ru.stqa.pft.addressbook.test;
 
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
-    FirefoxDriver wd;
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+    WebDriver wd;
 
 
     @BeforeMethod
