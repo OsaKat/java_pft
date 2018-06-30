b1a835d54cb736b60ca91507e1c0ec8018da897b
OsaKat <ekaterinaospennikova@gmail.com>
2018-06-30 19:55:59 +0300

    Лекция 8.6. Получение почты на встроенный в тесты почтовый сервер

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
@@ -20,6 +20,6 @@ public class RegistrationHelper extends HelperBase {
         wd.get(confirmationLink);
         type(By.name("password"), password);
         type(By.name("password_confirm"), password);
-        click(By.cssSelector("input[value='Изменить учетную запись']"));
+        click(By.cssSelector("button.width-100"));
     }
 }
